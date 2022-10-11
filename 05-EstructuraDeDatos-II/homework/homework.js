d6dc05b10dc4252c0536c497f637361b35f9ec20
hernandezcarlos04 <garciah.carlose09@gmail.com>
2022-10-11 15:41:30 -0600

    hw_Estructura_Dato2

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -3,17 +3,84 @@
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  - remove: elimina el último nodo de la lista y retorna su valor 
+    (tener en cuenta el caso particular de una lista de un solo 
+    nodo y de una lista vacía);
+  - search: recibe un parámetro y lo busca dentro de la lista, 
+    con una particularidad: el parámetro puede ser un valor o un 
+    callback. En el primer caso, buscamos un nodo cuyo valor coincida 
+    con lo buscado; en el segundo, buscamos un nodo cuyo valor, al 
+    ser pasado como parámetro del callback, retorne true. 
   Ejemplo: 
   search(3) busca un nodo cuyo valor sea 3;
-  search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
-  En caso de que la búsqueda no arroje resultados, search debe retornar null.
+  search(isEven), donde isEven es una función que retorna true 
+  cuando recibe por parámetro un número par, busca un nodo cuyo 
+  valor sea un número par.
+  En caso de que la búsqueda no arroje resultados, search debe 
+  retornar null.
 */
 
-function LinkedList() {}
+function Node(value) {
+  this.value =value;
+  this.next = null;
+}
 
-function Node(value) {}
+function LinkedList() {
+  this.head = null;
+}
+
+LinkedList.prototype.add=function(val){
+  let node = new Node(val); //Se crea la instancia del nodo
+  if(!this.head){
+    this.head = node;
+  }else{
+    let ndoActual = this.head;
+    
+    while(ndoActual.next){
+      ndoActual = ndoActual.next;
+    }
+    ndoActual.next = node;
+  }
+
+  
+
+}
+LinkedList.prototype.remove=function(){
+  let dele = this.head;
+  if(dele === null) return null;
+  else if(this.head && !this.head.next){
+    let aux = this.head.value;
+    this.head = null;
+    return aux;
+  }
+    while(dele.next){
+       if(dele.next.next === null){
+          let valor = dele.next.value;
+          dele.next = null;
+          return valor;
+      }
+      dele = dele.next;
+    }
+    
+  
+}
+LinkedList.prototype.search = function(value){
+  let ndoActual = this.head; 
+  if(this.head === null) return null;
+  while(ndoActual){
+    if(ndoActual.value === value) return ndoActual.value;
+    else if(typeof value == 'function'){
+      if(value(ndoActual.value)){
+        return ndoActual.value;
+      }
+    }
+    ndoActual = ndoActual.next;
+  }
+  return null;
+  
+}
+
+
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +97,33 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable() {
+  this.numBuckets = 35;
+  this.buckets = [];
+}
+HashTable.prototype.hash = function(key){
+  let sum =0;
+  for(let i=0; i<key.length; i++){
+    sum+=key.charCodeAt(i);//Tomando el valor de cada letra
+  }
+  return sum%this.numBuckets;
+};
+HashTable.prototype.set = function(key, value){
+  if(typeof key != 'string') throw TypeError('Keys must be string');
+  let i = this.hash(key);
+  if(this.buckets[i]=== undefined){
+    this.buckets[i]={};
+  }
+  this.buckets[i][key]= value;
+};
+HashTable.prototype.get = function(key){
+  let i = this.hash(key);
+  return this.buckets[i][key];
+};
+HashTable.prototype.hasKey = function(key){
+  let i = this.hash(key);
+  return this.buckets[i].hasOwnProperty(key);
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
